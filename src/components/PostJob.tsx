2684c4cd0f60a55aaeed06626755602cdd19449d
Dave <davearonmwan@gmail.com>
2024-08-12 20:59:53 +0100

    feat: add post job form

--- src/components/PostJob.tsx
+++ src/components/PostJob.tsx
@@ -1,5 +1,8 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Pencil } from "lucide-react";
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "./ui/button";
 import {
 	Dialog,
@@ -9,57 +12,206 @@ import {
 	DialogHeader,
 	DialogTrigger,
 } from "./ui/dialog";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+const jobSchema = z.object({
+	username: z.string().min(2).max(50),
+	displayName: z.string().min(2).max(100),
+	whatYouDo: z.string(),
+	location: z.string(),
+	pronouns: z.string(),
+	website: z.string().url({message: 'Please enter a valid website url'}),
+	about: z.string(),
+});
 
 export function PostJob({ trigger }: { trigger: ReactNode }) {
+	const form = useForm<z.infer<typeof jobSchema>>({
+		resolver: zodResolver(jobSchema),
+		defaultValues: {
+			username: "",
+			displayName: '',
+			whatYouDo: '',
+			location: '',
+			pronouns: '',
+			website: '',
+			about: '',
+		},
+	});
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof jobSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+
 	return (
 		<Dialog>
 			<DialogTrigger className="grow" asChild>
 				{trigger}
 			</DialogTrigger>
 			<DialogContent
-				className="p-0 border-divider-dark max-w-3xl"
+				className="p-0 border-divider-dark max-w-3xl pb-8"
 				hasCloseIcon={false}>
-				<DialogHeader className="p-4 space-y-[1.125rem] border-b border-b-divider-dark">
-					<div className="flex items-center gap-x-3">
-						<div className="aspect-square overflow-clip w-32">
-							<img
-								width={150}
-								height={150}
-								src="/images/following-demo.png"
-								alt="testing stuff"
-								className="w-full object-cover"
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="w-3/4">
+						<DialogHeader className="p-4 space-y-[1.125rem]">
+							<div className="flex items-center gap-x-3">
+								<div className="relative">
+									<div className="aspect-square overflow-clip w-20 rounded-full">
+										<img
+											width={150}
+											height={150}
+											src="/images/following-demo.png"
+											alt="testing stuff"
+											className="w-full h-full object-cover"
+										/>
+									</div>
+									<div className="absolute -bottom-2 right-0 p-2 rounded-full bg-block-light">
+										<input type="file" className="absolute inset-0 opacity-0" />
+										<Pencil size={18} className="stroke-white" />
+									</div>
+								</div>
+								<div>
+									<h2 className="text-xl leading-none font-medium text-white">
+										Name of user
+									</h2>
+									<p className="text-lg">@username</p>
+								</div>
+							</div>
+						</DialogHeader>
+						<DialogDescription className="p-4 space-y-6">
+							<div className="grid grid-cols-2 gap-4 items-center">
+								<FormField
+									control={form.control}
+									name="username"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Username</FormLabel>
+											<FormDescription>
+												This is your unique name.
+											</FormDescription>
+											<FormControl>
+												<Input {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="displayName"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Display name</FormLabel>
+											<FormDescription>
+												This is the name displayed on your profile.
+											</FormDescription>
+											<FormControl>
+												<Input {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+							<FormField
+								control={form.control}
+								name="whatYouDo"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>What do you do?</FormLabel>
+										<FormDescription>Your occupation.</FormDescription>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
 							/>
-						</div>
-						<div className="space-y-2">
-							<h2 className="text-xl font-medium text-white">Name of user</h2>
-							<p>@username</p>
-						</div>
-					</div>
-				</DialogHeader>
-				<DialogDescription className="p-4 space-y-4">
-					<h3 className="font-semibold text-[1.3125rem]">Description</h3>
-					<div className="space-y-3 text-[1.0625rem]">
-						<p>Senior UI/UX Designer</p>
-						<p> Design delightful experiences for our online platforms. </p>
-						<p>
-							At Procreate we're dedicated to making exceptional creative tools
-							by combining beautiful user experiences with high performance
-							engineering. Used by millions around the globe, Procreate is
-							committed to placing more power in the hands of creatives.{" "}
-						</p>
-						<p>
-							We're looking for a Senior UI/UX Designer to help design the look
-							and feel of Procreate's online platforms. In this role,
-							you&apos;ll work across a wide range of digital experiences with
-							the view to refine and extend our established on-line presence.
-						</p>
-					</div>
-				</DialogDescription>
-				<DialogFooter className="p-0 px-4 pb-4">
-					<Button size={"lg"} className="w-full" variant={"secondary"} asChild>
-						<Link to="/jobs/12dddeax">Open this position</Link>
-					</Button>
-				</DialogFooter>
+							<FormField
+								control={form.control}
+								name="location"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Location</FormLabel>
+										<FormDescription>
+											This is where you are based.
+										</FormDescription>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="pronouns"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Pronouns</FormLabel>
+										<FormDescription>
+											This is what you want others to know you as. E.g she/her,
+											he/him etc
+										</FormDescription>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="website"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Website</FormLabel>
+										<FormDescription>
+											A link to your portfolio, blog or personal website.
+										</FormDescription>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="about"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>About</FormLabel>
+										<FormDescription>
+											A short bio about yourself
+										</FormDescription>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</DialogDescription>
+						<DialogFooter className="p-0 px-4 pb-4 pt-6">
+							<Button className="w-full">Done</Button>
+							<Button className="w-full" variant={"secondary"} type="button">
+								Save to draft
+							</Button>
+						</DialogFooter>
+					</form>
+				</Form>
 			</DialogContent>
 		</Dialog>
 	);

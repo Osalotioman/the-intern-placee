060946404936800e406c294436e29769849e829c
Dave <davearonmwan@gmail.com>
2025-01-13 16:34:27 +0100

    fix: env variable name

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ const projectId = checkEnv("VITE_FIREBASE_PROJECT_ID");
 const storageBucket = checkEnv("VITE_FIREBASE_STORAGE_BUCKET");
 const messagingSenderId = checkEnv("VITE_FIREBASE_MESSAGING_ID");
 const appId = checkEnv("VITE_FIREBASE_APP_ID");
-const measurementId = checkEnv("VITE_FIREBSE_MEASUREMENT_ID");
+const measurementId = checkEnv("VITE_FIREBASE_MEASUREMENT_ID");
 
 const storageBucketUrl = checkEnv("VITE_FIREBASE_STORAGE_BUCKET_URL")
 
